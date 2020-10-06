e2521fbc018e02428ce0f3fb87b164bbd68eab6f
fj <castelloiv@gmail.com>
2020-10-06 16:07:45 -0400

    bootstrap added, custom scss

---
+++ main.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("button");
+
+function water() {
+  let xhr = new XMLHttpRequest();
+  /// Open function, takes in type, url/file, async or not;
+  console.log(xhr);
+}
+
+button.addEventListener("click", water);
